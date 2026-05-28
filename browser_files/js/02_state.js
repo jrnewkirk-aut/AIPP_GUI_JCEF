2b416119d357b4ddd6cea127ad329fce35db039c
jrnewkirk-aut <117690085+jrnewkirk-aut@users.noreply.github.com>
2026-05-27 22:12:54 -0600

    improve chamber visualization

--- browser_files/js/02_state.js
+++ browser_files/js/02_state.js
@@ -1,2 +1,3 @@
-let fullJson=null,currentTab="tree",treeOpenAll=false,treeUserOpen=new Set(),selectedJsonPath=null;
+let fullJson=null,currentTab="tree",treeOpenAll=false,treeUserOpen=new Set();
 let simNodes=[],simEdges=[],popupNode=null,popupPath=null,popupWorkingCopy=null,popupOriginalCopy=null,pendingCdCsvImport=null;
+let flowViewBox={x:0,y:0,w:1000,h:700,base:null},flowPan={active:false,x:0,y:0,start:null};
--- browser_files/js/16_init.js
+++ browser_files/js/16_init.js
@@ -1,1 +1,1 @@
-window.onload=function(){renderTree();updateCodeTextFromModel();attachCodeEditorHandlers();setStatus("Ready. Click Open JSON File.",true);window.toScilab("loaded");};
+window.onload=function(){renderTree();updateCodeTextFromModel();attachCodeEditorHandlers();initFlowZoomControls();setStatus("Ready. Click Open JSON File.",true);window.toScilab("loaded");};
